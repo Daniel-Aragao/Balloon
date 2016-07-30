3344c86744c65f28565a8d05fa16add15c63b3ff
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-07-30 00:45:32 -0300

    checkpoint refatoramento 2

--- balloon.js
+++ balloon.js
@@ -1,129 +1,135 @@
-var container;
-var default_options = { // for no option selected
-    position_x: 'top', //screen position
-    position_y: 'right', //screen position
-    type: 'primary', // bootstrap same types
-    closeButton: false, // no close btn
-    timeOut: 0, //no time out
-    clickToClose: true
-}
 
-var configuracao = {} 
 
 
 function balloon(msg, btype, options) {    
 
-    SetConfiguration(options);
+    balloonInflater.SetConfiguration(options);
 
-    CreateContainer();
+    balloonInflater.CreateContainer();
         
     var ballon = $('<div class="balloon"></div>');
     var message = $('<div class="balloon-message"></div>');
     message.text(msg);
 
-    AdicionarClasses(btype, ballon);
-    AdicionarCloseBtn(ballon)
+    balloonInflater.AdicionarClasses(btype, ballon);
+    balloonInflater.AdicionarCloseBtn(ballon)
+
     ballon.append(message);
 
-    TempoLimite(ballon);
-    ClicarFechar(ballon);
+    balloonInflater.TempoLimite(ballon);
+    balloonInflater.ClicarFechar(ballon);
 
-    container.append(ballon);
+    balloonInflater.container.append(ballon);
     return ballon;
 }
 
-function CreateContainer() {
-    container = $("#balloon-container");
-    
-    if (container.length === 0) {
-        container = $('<div id="balloon-container"><div>')
-        ContainerPosition();
-        $('body').append(container);
-    }
-}
+var balloonInflater = {
+    container: null,
 
-function ClicarFechar(ballon) {    
-    if (configuracao.clickToClose == true || configuracao.clickToClose == "true") {       
-        ballon.on('click', function () {
-            ballon.remove();
-        })
-    }
-}
-
-function ContainerPosition() { 
-    container.addClass('balloon-' + configuracao.position_x)
-    container.addClass('balloon-'+configuracao.position_y)
-}
-
-function AdicionarCloseBtn(ballon) {
-    
-    if (configuracao.closeButton == true || configuracao.closeButton == "true") {
-        var btn = $('<div class="closeBtn">x</div>');
+    CreateContainer: function () {
+        this.container = $("#balloon-container");
         
-        btn.on('click', function () {
-            ballon.remove(); 
-        });
+        if (this.container.length === 0) {
+            this.container = $('<div id="balloon-container"><div>')
+            this.ContainerPosition();
+            $('body').append(this.container);
+        }
+    },
 
-        ballon.append(btn);
-    }
-}
+    default_options : { // for no option selected
+        position_x: 'top', //screen position
+        position_y: 'right', //screen position
+        type: 'primary', // bootstrap same types
+        closeButton: false, // no close btn
+        timeOut: 0, //no time out
+        clickToClose: true
+    },
 
-function TempoLimite(ballon) {   
-    if (configuracao.timeOut != 0) {
-        setTimeout(function () { 
-            ballon.remove()
-        } ,configuracao.timeOut)
-    }
-}
+    configuracao: {},
+    
+    ClicarFechar: function (ballon) {    
+        if (this.configuracao.clickToClose == true || this.configuracao.clickToClose == "true") {       
+            ballon.on('click', function () {
+                ballon.remove();
+            })
+        }
+    },
 
-function AdicionarClasses(btype, balloon) {
-    if (typeof btype == "undefined" || btype == null) {
-	    btype = configuracao.type; 
-    } 
+    ContainerPosition: function () { 
+        this.container.addClass('balloon-' + this.configuracao.position_x)
+        this.container.addClass('balloon-'+ this.configuracao.position_y)
+    },
+
+    AdicionarCloseBtn: function (ballon) {
+        
+        if (this.configuracao.closeButton == true || this.configuracao.closeButton == "true") {
+            var btn = $('<div class="closeBtn">x</div>');
+            
+            btn.on('click', function () {
+                ballon.remove(); 
+            });
+
+            ballon.append(btn);
+        }
+    },
+
+    TempoLimite: function (ballon) {   
+        if (this.configuracao.timeOut != 0) {
+            setTimeout(function () { 
+                ballon.remove()
+            } ,this.configuracao.timeOut)
+        }
+    },
+
+    AdicionarClasses: function (btype, balloon) {
+        if (typeof btype == "undefined" || btype == null) {
+            btype = this.configuracao.type; 
+        } 
 
 
-    switch (btype) {
-        case "success":
-            balloon.addClass("balloon-success");
+        switch (btype) {
+            case "success":
+                balloon.addClass("balloon-success");
+                break;
+            case "info":
+                balloon.addClass("balloon-info");
             break;
-        case "info":
-            balloon.addClass("balloon-info");
-        break;
-        case "warning":
-            balloon.addClass("balloon-warning");
-            break;
-        case "danger":
-            balloon.addClass("balloon-danger");
-            break;
-        case "primary":
-            balloon.addClass("balloon-primary");
-            break;
-        case "balloon":
-            balloon.addClass("balloon-balloon");
-            break;
-        default:
-            break;
-    }
-}
+            case "warning":
+                balloon.addClass("balloon-warning");
+                break;
+            case "danger":
+                balloon.addClass("balloon-danger");
+                break;
+            case "primary":
+                balloon.addClass("balloon-primary");
+                break;
+            case "balloon":
+                balloon.addClass("balloon-balloon");
+                break;
+            default:
+                break;
+        }
+    },
 
-function SetConfiguration(options) {
-    if (isNullOrUndefined(options)) {
-        options = default_options;
-    }
+    SetConfiguration: function (options) {
+        if (this.isNullOrUndefined(options)) {
+            options = this.default_options;
+        }
 
-    for (var attr in default_options) {configuracao[attr] = default_options[attr];} 
+        for (var attr in this.default_options) {this.configuracao[attr] = this.default_options[attr];} 
 
-    if (default_options != options) {
-        for (var attr in options) {
-            if(!isNullOrUndefined(options[attr])) configuracao[attr] = options[attr];
+        if (this.default_options != options) {
+            for (var attr in options) {
+                if(!this.isNullOrUndefined(options[attr])) this.configuracao[attr] = options[attr];
+            }    
         }    
-    }    
 
-}
+    },
 
-function isNullOrUndefined(variavel) {
-    if (typeof variavel == "undefined" || !variavel) {
-        return true;
+    isNullOrUndefined: function (variavel) {
+        if (typeof variavel == "undefined" || !variavel) {
+            return true;
+        }
+        return false;
     }
-    return false;
 }
